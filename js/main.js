6c910c038f63377ad673dad27c25de08d9feb52f
Matias Roces <matias.roces.web@gmail.com>
2023-08-20 12:17:24 -0300

    2da entrega TP

--- js/main.js
+++ js/main.js
@@ -1,33 +1,67 @@
 //JS V1.0
 
-alert("Bienvenido a La Vie Est Belle, a continuación, se detallan los códigos de los productos que desea añadir al carrito: \n1-Jabón perfumado......($700)\n2-Perfuminas.................($1200) \n3-Velas aromáticas......($1400) \n4-Sales de baño..............($850)\n5-Apresto para ropa....($1500)\n\n Por favor lea detenidamente los códigos de los productos y prepárese para realizar su pedido. Al final, se le devolverá el monto final de la compra.");
-
 //declaración de funciones
-
-function producto(num1, num2){
-    let resultado = (parseFloat(num1))*(parseFloat(num2));
-    return resultado;
-}
-function pedirCantidad(){
-    let numero = false;
-    while (!numero){
-        cantidad = parseFloat(prompt("Ingrese la cantidad del producto que desea adquirir:"));
-        numero = !isNaN(cantidad);
-        if (numero && (cantidad > 0)){
-            return cantidad;
-            break;
-        }
-        else{
-            alert("Ha habido un error al intentar leer la cantidad. Escriba la cantidad en numeros naturales, por favor");
+function articulos(id, nombre, precio, stock){
+    //atributos
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.contador = 0;
+    this.totalProducto = 0;
+    // metodos
+    this.mostrarStock = function(){
+        alert(`El stock disponible de ${this.nombre} es ${this.stock}, por favor ingrese una cantidad que no supere nuestro stock.`)
+    }
+    this.stockNow = function(cant){
+        if (!isNaN(cant)){
+            this.stock-= cant;
+        }                 
+    }
+    this.calcularCompra = function(){
+        this.totalProducto = this.contador * this.precio;
+        return this.totalProducto;
+    }
+    // metodo de validación de cantidad positiva y que no supere stock
+    this.cantidadCompra = function(){
+        let numero = false;
+        while (!numero){
+            let cant = parseFloat(prompt("Ingrese la cantidad del producto que desea adquirir:"));
+            numero = !isNaN(cant);
+            if (numero && (cant > 0)){
+                if (cant <= this.stock){
+                    this.contador+=cant;                
+                    return cant;
+                }
+                else{
+                    this.mostrarStock();
+                }                                    
+            }
+            else {
+                alert("Ha habido un error al intentar leer la cantidad. Escriba la cantidad en numeros naturales, por favor");
+            }
+            
         }
     }
+    
 }
 
 //declaración de variables
+const arrayProductos = new Array();
+const jabonPerfumado = new articulos(1, "Jabon Perfumado", 700, 10);
+arrayProductos.push(jabonPerfumado);
+const perfumina = new articulos(2, "Perfuminas", 1200, 20);
+arrayProductos.push(perfumina);
+const velasAromaticas = new articulos(3, "Velas Aromaticas", 1400, 8);
+arrayProductos.push(velasAromaticas);
+const salesBaño = new articulos(4, "Sales de Baño", 850, 5);
+arrayProductos.push(salesBaño);
+const aprestoRopa = new articulos(5, "Apresto para Ropa", 1500, 3);
+arrayProductos.push(jabonPerfumado);
 
-const jabonPerfumado = 700, perfumina = 1200, velasAromaticas = 1400, salesBaño = 850, aprestoRopa = 1500;
-let contador1 = 0, contador2 = 0, contador3 = 0, contador4 = 0, contador5 = 0;
-let totalCarrito = 0;
+//mensaje inicial
+alert(`Bienvenido a La Vie Est Belle, a continuación, se detallan los códigos de los productos que desea añadir al carrito: \n1-${arrayProductos[0].nombre} (Stock: ${arrayProductos[0].stock})......($${arrayProductos[0].precio})\n2-${arrayProductos[1].nombre} (Stock: ${arrayProductos[1].stock}).................($${arrayProductos[1].precio}) \n3-${arrayProductos[2].nombre} ((Stock: ${arrayProductos[2].stock}))......($${arrayProductos[2].precio}) \n4-${arrayProductos[3].nombre} (Stock: ${arrayProductos[3].stock})..............($${arrayProductos[3].precio})\n5-${arrayProductos[4].nombre} (Stock: ${arrayProductos[4].stock})....($${arrayProductos[4].precio})\n\n Por favor lea detenidamente los códigos de los productos y prepárese para realizar su pedido. Al final, se le devolverá el monto final de la compra. Si no desea comprar, o si ya finalizó su compra, ingrese el codigo 0.`);
+
 let codigoProducto = 0;
 let cantidad;
 
@@ -35,36 +69,39 @@ do{
     codigoProducto = parseFloat(prompt("Ingrese el código del producto que desea añadir al carrito. Si no desea realizar una compra o ya eligió todos los productos, ingrese el numero 0"));
     switch (codigoProducto){
         case 1:
-            cantidad = pedirCantidad();
-            totalCarrito+= producto(jabonPerfumado, cantidad);
-            contador1+=cantidad;
+            cantidad = arrayProductos[0].cantidadCompra();
+            arrayProductos[0].stockNow(cantidad);            
             break;
         case 2:
-            cantidad = pedirCantidad();
-            totalCarrito+= producto(perfumina, cantidad);
-            contador2+=cantidad;
+            cantidad = arrayProductos[1].cantidadCompra();
+            arrayProductos[1].stockNow(cantidad);            
             break;
         case 3:
-            cantidad = pedirCantidad();
-            totalCarrito+= producto(velasAromaticas, cantidad);
-            contador3+=cantidad;
+            cantidad = arrayProductos[2].cantidadCompra();
+            arrayProductos[2].stockNow(cantidad);         
             break;
         case 4:
-            cantidad = pedirCantidad();
-            totalCarrito+= producto(salesBaño, cantidad);
-            contador4+=cantidad;
+            cantidad = arrayProductos[3].cantidadCompra();
+            arrayProductos[3].stockNow(cantidad);           
             break;
         case 5:
-            cantidad = pedirCantidad();
-            totalCarrito+= producto(aprestoRopa, cantidad);
-            contador5+=cantidad;
+            cantidad = arrayProductos[4].cantidadCompra();
+            arrayProductos[4].stockNow(cantidad);            
             break;
-        case 0: 
-            alert("Su factura final: \nJabones perfumados...."+contador1+"\nPerfuminas..............."+contador2+"\nVelas Aromaticas...."+contador3+"\nSales de Baño.........."+contador4+"\nApresto para ropa...."+contador5+"\n\nEl total de la compra ha sido $"+totalCarrito);
+        case 0:            
             break;
         default:
             alert("El código ingresado debe coincidir con algun producto, o si desea salir, recuerde ingresar 0");
     }
-
 }while(codigoProducto!=0);
-alert("Gracias por su compra en La Vie Est Belle");
+
+const totalCarrito = arrayProductos.reduce((acc, producto) => acc + producto.totalProducto, 0);
+
+alert(`Su factura final: \nJabones perfumados....${arrayProductos[0].calcularCompra()}"\nPerfuminas..............."${arrayProductos[1].calcularCompra()}"\nVelas Aromaticas....${arrayProductos[2].calcularCompra()}\nSales de Baño.........."${arrayProductos[3].calcularCompra()}"\nApresto para ropa...."${arrayProductos[4].calcularCompra()}"\n\nEl total de la compra ha sido $${totalCarrito}`);
+
+if (totalCarrito != 0){
+    alert("Gracias por su compra en La Vie Est Belle, vuelva prontos");
+}
+else {
+    alert("Recargue la página y compre algo, no sea rata.");
+}
