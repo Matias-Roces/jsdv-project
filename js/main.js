d9c73563fe9ea929a5b9fddd82cb19fe7307b54b
Matias Roces <matias.roces.web@gmail.com>
2023-09-05 22:20:47 -0300

    PRE ENTREGA 3

--- js/main.js
+++ js/main.js
@@ -17,32 +17,34 @@ class Producto {
     }
 }
 // declaracion de objetos
-const difusorAmb = new Producto(1, "Difusor ambiental", "Linea Home", "linea-home/difusor-ambiental.webp", 2799);
-const aceiteHidro = new Producto(2, "Aceite hidrosoluble", "Linea Home", "linea-home/aceites-hidrosolubles.webp", 4499);
-const perlasArom = new Producto(3, "Perlas aromáticas", "Linea Home", "linea-home/perlas-aromaticas.webp", 2349);
-const aceiteMasajes = new Producto(4, "Aceite para masajes", "Linea Spa", "linea-spa/aceite-para-masajes.webp", 1999);
-const aguaBanio = new Producto(5, "Agua de baño", "Linea Spa", "linea-spa/agua-de-bano.webp", 1899);
-const showerGel = new Producto(6, "Shower Gel", "Linea Spa", "linea-spa/shower-gel.webp", 1669);
-const boxJabones = new Producto(7, "Box de degustación de Jaboncitos", "Jabones", "jabones/box-degustacion-jabones.webp", 2699);
-const jabonPerfumado = new Producto(8, "Barra de jabon perfumado", "Jabones", "jabones/jabones-perfumados-frases.webp", 1399);
-const jabonesInfantiles = new Producto(9, "Corazoncitos infantiles", "Jabones", "jabones/jaboncitos-infantiles.webp", 2099);
-const jabonNatural = new Producto(10, "Jabon natural grande", "Jabones", "jabones/jabon-natural-grande.webp", 1999);
-const balsamoLabial = new Producto(11, "Bálsamo labial", "Cosmetica Natural", "cosmetica-natural/balsamo-labial.webp", 1399);
-const blanqueadorDental = new Producto(12, "Blanqueador dental", "Cosmetica Natural", "cosmetica-natural/blanqueador-dental.webp", 1799);
-const shampooNeutro = new Producto(13, "Shampoo neutro", "Cosmetica Natural", "cosmetica-natural/shampoo-neutro.webp", 2699);
-const esponjaVegetal = new Producto(14, "Esponja vegetal", "Esponjas", "esponjas/esponja-vegetal.webp", 3199);
-const esponjaCapullo = new Producto(15, "Esponja capullo", "Esponjas", "esponjas/esponja-capullo.webp", 2799);
-const almohadillaCervical = new Producto(16, "Almohadilla cervical", "Almohadillas y Antifaces", "almohadilla-antifaz/almohadilla-cervical.webp", 3699);
-const almohadillaOcular = new Producto(17, "Almohadilla térmica ocular", "Almohadillas y Antifaces", "almohadilla-antifaz/almohadilla-termica-ocular.webp", 2899);
-const almohadillaViaje = new Producto(18, "Almohadilla de viaje", "Almohadillas y Antifaces", "almohadilla-antifaz/almohadilla-viaje.webp", 4299);
-const tapaluzOcular = new Producto(19, "Tapaluz ocular", "Almohadillas y Antifaces", "almohadilla-antifaz/tapaluz-ocular.webp", 2299);
-const portaCepillos = new Producto(20, "Porta cepillos de madera", "Accesorios de madera", "accesorios-madera/porta-cepillos.webp", 2749);
-const jaboneraMadera = new Producto(21, "Jabonera de madera virgen", "Accesorios de madera", "accesorios-madera/jabonera-madera-v.webp", 2649);
-const bandejaTaco = new Producto(22, "Bandeja y taco de madera", "Accesorios de madera", "accesorios-madera/bandeja-taco-madera.webp", 2479);
+const difusorAmb = new Producto(1, "DIFUSOR AMBIENTAL", "Linea Home", "linea-home/difusor-ambiental.webp", 2799);
+const aceiteHidro = new Producto(2, "ACEITE HIDROSOLUBLE", "Linea Home", "linea-home/aceites-hidrosolubles.webp", 4499);
+const perlasArom = new Producto(3, "PERLAS AROMATICAS", "Linea Home", "linea-home/perlas-aromaticas.webp", 2349);
+const aceiteMasajes = new Producto(4, "ACEITE PARA MASAJES", "Linea Spa", "linea-spa/aceite-para-masajes.webp", 1999);
+const aguaBanio = new Producto(5, "AGUA DE BAÑO", "Linea Spa", "linea-spa/agua-de-bano.webp", 1899);
+const showerGel = new Producto(6, "SHOWER GEL", "Linea Spa", "linea-spa/shower-gel.webp", 1669);
+const boxJabones = new Producto(7, "BOX DE JABONCITOS", "Jabones", "jabones/box-degustacion-jabones.webp", 2699);
+const jabonPerfumado = new Producto(8, "BARRA JABON PERFUMADO", "Jabones", "jabones/jabones-perfumados-frases.webp", 1399);
+const jabonesInfantiles = new Producto(9, "CORAZONCITOS INFANTILES", "Jabones", "jabones/jaboncitos-infantiles.webp", 2099);
+const jabonNatural = new Producto(10, "JABON NATURAL GRANDE", "Jabones", "jabones/jabon-natural-grande.webp", 1999);
+const balsamoLabial = new Producto(11, "BALSAMO LABIAL", "Cosmetica Natural", "cosmetica-natural/balsamo-labial.webp", 1399);
+const blanqueadorDental = new Producto(12, "BLANQUEADOR DENTAL", "Cosmetica Natural", "cosmetica-natural/blanqueador-dental.webp", 1799);
+const shampooNeutro = new Producto(13, "SHAMPOO NEUTRO", "Cosmetica Natural", "cosmetica-natural/shampoo-neutro.webp", 2699);
+const esponjaVegetal = new Producto(14, "ESPONJA VEGETAL", "Esponjas", "esponjas/esponja-vegetal.webp", 3199);
+const esponjaCapullo = new Producto(15, "ESPONJA CAPULLO", "Esponjas", "esponjas/esponja-capullo.webp", 2799);
+const almohadillaCervical = new Producto(16, "ALMOHADILLA CERVICAL", "Almohadillas y Antifaces", "almohadilla-antifaz/almohadilla-cervical.webp", 3699);
+const almohadillaOcular = new Producto(17, "ALMOHADILLA TERMICA OCULAR", "Almohadillas y Antifaces", "almohadilla-antifaz/almohadilla-termica-ocular.webp", 2899);
+const almohadillaViaje = new Producto(18, "ALMOHADILLA DE VIAJE", "Almohadillas y Antifaces", "almohadilla-antifaz/almohadilla-viaje.webp", 4299);
+const tapaluzOcular = new Producto(19, "TAPALUZ OCULAR", "Almohadillas y Antifaces", "almohadilla-antifaz/tapaluz-ocular.webp", 2299);
+const portaCepillos = new Producto(20, "PORTA CEPILLOS DE MADERA", "Accesorios de madera", "accesorios-madera/porta-cepillos.webp", 2749);
+const jaboneraMadera = new Producto(21, "JABONERA DE MADERA VIRGEN", "Accesorios de madera", "accesorios-madera/jabonera-madera-v.webp", 2649);
+const bandejaTaco = new Producto(22, "BANDEJA Y TACO DE MADERA", "Accesorios de madera", "accesorios-madera/bandeja-taco-madera.webp", 2479);
 
 const busqueda = document.querySelector("#buscarProducto");
 const catalogo = document.querySelector("#catalogo");
 const selectCategoria = document.querySelector("#selectCategoria");
+const textBoxBuscar = document.querySelector("#textBoxBuscar");
+
 
 // funcion que renderiza las cards
 function crearCards(arr) {
@@ -76,7 +78,14 @@ selectCategoria.addEventListener("change", () => selectCategoria.value === "Todo
 
 console.log(filtroKey(listadoProductos, categoria[selectCategoria.value + 1], "categoria"));
 
-// crearCards(listadoProductos);
+busqueda.addEventListener("click", (event) => {
+    event.preventDefault();
+    crearCards(filtroKey(listadoProductos, textBoxBuscar.value.toUpperCase(), "nombre"));
+});
+
+textBoxBuscar.addEventListener("clear",()=> textBoxBuscar.value == "" && crearCards(listadoProductos));
 
 
-// console.log(listadoProductos);
+
+
+
